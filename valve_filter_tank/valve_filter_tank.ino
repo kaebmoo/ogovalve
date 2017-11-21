faf450fcea05989a3770b36817e5fa5b401d117d
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2017-11-21 20:03:42 +0700

    UNO & WEMOS
    Add wemos version

--- valve_filter_tank/valve_filter_tank.ino
+++ valve_filter_tank/valve_filter_tank.ino
@@ -4,10 +4,19 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
 
-// UNO 7 segments display
-#define CLK 9
-#define DIO 8
+// #define UNO 
 
+// UNO 7 segments display
+// #define CLK 9
+// #define DIO 8
+// int ledPin = 13;       // the number of the output pin
+// byte buttons[] = {2, 3, 4, 5, 6};
+
+// Wemos D1 mini
+#define CLK D3
+#define DIO D4
+int ledPin = D4;
+byte buttons[] = {D0, D5, D6, D7};  // switch 
 
 TM1637Display display(CLK, DIO);
 
@@ -19,12 +28,8 @@ TM1637Display display(CLK, DIO);
 
 Adafruit_MCP23008 mcp;
 
-int buttonPin = 2;         // the number of the input pin
-int stopPin = 3;
-int ledPin = 13;       // the number of the output pin
 
 #define DEBOUNCE 10  // button debouncer, how many ms to debounce, 5+ ms is usually plenty
-byte buttons[] = {2, 3, 4, 5, 6};
 
 // This handy macro lets us determine how big the array up above is, by checking the size
 #define NUMBUTTONS sizeof(buttons)
@@ -45,6 +50,9 @@ unsigned long previousMillis = 0;        // will store last time LED was updated
 // constants won't change:
 const long interval = 1000;           // interval at which to blink (milliseconds)
 int ledState = LOW;             // ledState used to set the LED
+
+// wemos 
+int wemosautostart = 1;
   
 void setup()
 {
@@ -64,7 +72,8 @@ void setup()
   display.setSegments(data);
   
   pinMode(ledPin, OUTPUT);
-  
+
+  Serial.println();
   Serial.print("Button checker with ");
   Serial.print(NUMBUTTONS, DEC);
   Serial.println(" buttons");
@@ -74,12 +83,12 @@ void setup()
     pinMode(buttons[i], INPUT);
     digitalWrite(buttons[i], HIGH);
   }
+  pinMode(D8, INPUT);
+  digitalWrite(D8, LOW);
   
   // mcp.begin(1); // address = 0 (valid: 0-7)
   setup_relayboard(0);
   
-  
-
   while (!Serial); // wait for serial port to connect. Needed for Leonardo only
   Serial.println("I2C Relayboard test - press keys 12345678 (toggle relay) C (clear all)");
   relay_reset();
@@ -118,6 +127,7 @@ void loop()
       data[3] = 0x50;
       display.setSegments(data);
       ledState = HIGH;
+      //Serial.println(digitalRead(D8));    // wemos input 
     } else {
       data[0] = 0x00;
       data[1] = 0x00;
@@ -169,6 +179,19 @@ void loop()
     case 5: 
       Serial.println("switch 6 just pressed"); break;    
   }
+
+  // wemos auto start D8 input
+  if (digitalRead(D8) == 1 && wemosautostart == 1) {
+    Serial.println("switch 5 just pressed"); 
+    // automatic clean
+    setup_relayboard(0);
+    tank1_clean();
+    tank2_clean();
+    setup_relayboard(1);
+    tank3_clean();
+    tank4_clean();
+  }
+  // end wemos auto start 
   
   relay_serial_control();
   
